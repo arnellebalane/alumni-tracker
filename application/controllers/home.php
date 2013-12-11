ca9ca7ba7ea9ee973da91bbf96fa13fa0f841e83
Arnelle Balane <arnellebalane@gmail.com>
2013-12-11 17:28:18 +0800

    thank you page after submission of answers

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,14 +3,18 @@
   class Home extends CI_Controller {
 
   	public function index() {  		
-  		$this->load->view("index.php");
+  		$this->load->view("index");
   	}
 
   	public function questionnaire() {
   		$this->load->model("values_model", "model");
   		$data = array('countries'=>$this->model->getCountries());
-  		$this->load->view("questionnaire.php", $data);
+  		$this->load->view("questionnaire", $data);
   	}
+
+    public function saved() {
+      $this->load->view('alumni_saved');
+    }
 
   }
 
---
+++ application/views/alumni_saved.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="<?= base_url() . 'assets/stylesheets/reset.css'; ?>" />
+  <link rel="stylesheet" href="<?= base_url() . 'assets/stylesheets/fonts.css'; ?>" />
+  <link rel="stylesheet" href="<?= base_url() . 'assets/stylesheets/application.css'; ?>" />
+  <link rel="stylesheet" href="<?= base_url() . 'assets/stylesheets/pages.css'; ?>" />
+  <script src="<?= base_url() . 'assets/javascripts/jquery-2.js'; ?>"></script>
+  <script src="<?= base_url() . 'assets/javascripts/application.js'; ?>"></script>
+  <title>Alumni Tracker</title>
+</head>
+
+<body class="pages saved">
+  <h1>Thank you!</h1>
+  <p>Your answers has been saved to our records. To modify your information in the future, you have to log in to your account. Your login information are shown below:</p>
+  <div id="alumni-account">
+    <label>Username:</label><span>2011-37575</span>
+    <label>Password:</label><span>SGHeKH</span>
+  </div>
+  <a href="#">Login to Account</a>
+</body>
+</html>

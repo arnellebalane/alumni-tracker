4caeb0e27675431c35028e6b9421c820c922d132
Arnelle Balane <arnellebalane@gmail.com>
2014-01-15 21:13:27 +0800

    clean alumni data page in admin pages

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -5,15 +5,15 @@
     public function __construct() {
       parent::__construct();
       if ($this->session->userdata('user_type') != "super admin") {
-      	redirect('/session/index');
+        redirect('/session/index');
       }
       $this->load->model("values_model", "values");
     }
 
-    public function index() {    	
-    	$data = array('countries' => $this->values->getCountries(),
-    								'employer_types'=>$this->values->getEmployerTypes(), 
-    								'programs'=>$this->values->getPrograms(),
+    public function index() {      
+      $data = array('countries' => $this->values->getCountries(),
+                    'employer_types'=>$this->values->getEmployerTypes(), 
+                    'programs'=>$this->values->getPrograms(),
                     'ge_courses'=>$this->values->getGECourses(),
                     'social_networks' => $this->values->getSocialNetworks());
       $this->load->view('admin/index', $data);
@@ -23,60 +23,64 @@
       $this->load->view('admin/alumni');
     }
 
+    public function clean($id) {
+      $this->load->view('admin/clean');
+    }
+
     public function addCountry() {
-    	if ($_POST['country_name'] == "") {
-    		$this->session->set_flashdata("alert", "The country name should not be blank!");    	
-    	}  else {
-    	    $this->values->addCountry($_POST['country_name']);
+      if ($_POST['country_name'] == "") {
+        $this->session->set_flashdata("alert", "The country name should not be blank!");      
+      }  else {
+          $this->values->addCountry($_POST['country_name']);
             $this->session->set_flashdata("notice", "New country added!");
         }
-    	redirect('/admin/index');
+      redirect('/admin/index');
     }
 
     public function updateCountries() {
-    	if ($_POST['country_name'] == "") {
-    		$this->session->set_flashdata("alert", "The country name should not be blank!");    		
-    	}  else {
-        	$id = $this->values->addCountry($_POST['country_name']);
-        	foreach($_POST['countries'] as $key => $value) {
-        		$this->values->replaceCountry($id, $key);
-        	}
-        	$this->session->set_flashdata("notice", "The countries where replaced successfully!");
+      if ($_POST['country_name'] == "") {
+        $this->session->set_flashdata("alert", "The country name should not be blank!");        
+      }  else {
+          $id = $this->values->addCountry($_POST['country_name']);
+          foreach($_POST['countries'] as $key => $value) {
+            $this->values->replaceCountry($id, $key);
+          }
+          $this->session->set_flashdata("notice", "The countries where replaced successfully!");
         }
-    	redirect('/admin/index');
+      redirect('/admin/index');
     }
 
     public function addEmployerType() {
-    	if ($_POST['employer_type'] == "") {
-    		$this->session->set_flashdata("alert", "The type name should not be blank!");    		
-    	}  else {
-        	$this->values->addEmployerType($_POST['employer_type']);
-        	$this->session->set_flashdata("notice", "New employer type added!");
+      if ($_POST['employer_type'] == "") {
+        $this->session->set_flashdata("alert", "The type name should not be blank!");        
+      }  else {
+          $this->values->addEmployerType($_POST['employer_type']);
+          $this->session->set_flashdata("notice", "New employer type added!");
         }
-    	redirect('/admin/index');
+      redirect('/admin/index');
     }
 
     public function updateEmployerTypes() {
-    	if ($_POST['employer_type'] == "") {
-    		$this->session->set_flashdata("alert", "The type name should not be blank!");    		
-    	}  else {
-        	$id = $this->values->addEmployerType($_POST['employer_type']);
-        	foreach($_POST['employer_types'] as $key => $value) {
-        		$this->values->replaceEmployerType($id, $key);
-        	}
-        	$this->session->set_flashdata("notice", "The employer types were replaces successfully!");
+      if ($_POST['employer_type'] == "") {
+        $this->session->set_flashdata("alert", "The type name should not be blank!");        
+      }  else {
+          $id = $this->values->addEmployerType($_POST['employer_type']);
+          foreach($_POST['employer_types'] as $key => $value) {
+            $this->values->replaceEmployerType($id, $key);
+          }
+          $this->session->set_flashdata("notice", "The employer types were replaces successfully!");
         }
-    	redirect('/admin/index');
+      redirect('/admin/index');
     }
 
     public function addDegreeProgram() {
-    	if ($_POST['degree_program'] == "") {
-    		$this->session->set_flashdata("alert", "The degree program should not be blank!");    	
-    	}  else {
-        	$this->values->addProgram($_POST['degree_program']);
-        	$this->session->set_flashdata("notice", "New degree program added!");
+      if ($_POST['degree_program'] == "") {
+        $this->session->set_flashdata("alert", "The degree program should not be blank!");      
+      }  else {
+          $this->values->addProgram($_POST['degree_program']);
+          $this->session->set_flashdata("notice", "New degree program added!");
         }
-    	redirect('/admin/index');
+      redirect('/admin/index');
     }
 
     public function updateDegreeProgram() {

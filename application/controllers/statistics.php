9629f9a309b4ef643edd3651327f48e49bf15d63
Arnelle Balane <arnellebalane@gmail.com>
2014-01-25 22:03:50 +0800

    statistical presentation for employment gap variable

--- application/controllers/statistics.php
+++ application/controllers/statistics.php
@@ -82,6 +82,10 @@
       $this->load->view('statistics/self_employed');
     }
 
+    public function employment_gap() {
+      $this->load->view('statistics/employment_gap');
+    }
+
     public function generate_pdf() {
       $this->load->add_package_path(APPPATH . 'libraries/mpdf');
       $this->load->library('mpdf');

ab39d957c24258837d87dbec4e9a6e3c6e09be2e
Paulinko Kiel Labian <paw2geek23@gmail.com>
2014-01-27 11:51:32 +0800

    added view excel.php

---
+++ application/views/excel.php
@@ -0,0 +1,101 @@
+<?php
+header("Content-type: application/vnd.ms-excel");
+header("Content-Disposition: attachment;Filename=Alumni Data.xls");
+?>
+
+<html>
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset=Windows-1252\">
+<head>
+	<style>
+		td	{
+			text-align: left;
+		}
+		.center td {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<table border="1">
+		<tr class="center">
+			<td></td>
+			<td colspan="9">PERSONAL INFO</td>
+			<td colspan="5">EDUCATIONAL BACKGROUND</td>
+			<td colspan="11">EMPLOYMENT HISTORY</td>
+		</tr>
+		<tr class="center">
+			<td>#</td>
+			<td>LAST NAME</td>
+			<td>FIRST NAME</td>
+			<td>GENDER</td>
+			<td>PRESENT ADDRESS</td>
+			<td>COUNTRY</td>
+			<td>PRESENT CONTACT NUMBER</td>
+			<td>PERMANENT ADDRESS</td>
+			<td>PERMANENT CONTACT NUMBER</td>
+			<td>EMAIL</td>
+
+			<td>STUDENT NUMBER</td>
+			<td>PROGRAM</td>
+			<td>AY GRADUATED</td>
+			<td>SEMESTER GRADUATED</td>
+			<td>HONOR RECEIVED</td>
+
+			<td>CURRENT JOB</td>
+			<td>FIRST JOB</td>
+			<td>SELF EMPLOYED</td>
+			<td>BUSINESS</td>
+			<td>EMPLOYER</td>
+			<td>JOB TYPE</td>
+			<td>JOB TITLE</td>
+			<td>MONTHLY SALARY</td>
+			<td>DURATION</td>
+			<td>JOB SATISFACTION</td>
+			<td>REASON</td>
+		</tr>
+
+		<?php $count = 0;  ?>
+		<?php foreach ($alumni as $var) : ?>
+			<tr class="non-center" valign="top">
+				<?php $rows = count($jobs[$count]); ?>
+				<td rowspan="<?= $rows ?>"><?= $count+1 ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->lastname ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->firstname ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->gender ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->present_address ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->country ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->present_contact_number ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->premanent_address ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->permanent_contact_number ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->email ?></td>
+
+				<td rowspan="<?= $rows ?>"><?= $var->student_number ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->program ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->year_graduated ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->semester_graduated ?></td>
+				<td rowspan="<?= $rows ?>"><?= $var->honor_received ?></td>
+
+			<?php for ($i=0; $i < $rows; $i++) : ?>
+				<?php $job = $jobs[$count][$i]; ?>
+				<?= ($i == 0) ? '':'<tr>'; ?>
+					<td><?= ($job->current_job == '1') ? 'yes': 'no'; ?></td>
+					<td><?= ($job->first_job == '1') ? 'yes': 'no'; ?></td>
+					<td><?= ($job->self_employed == '1') ? 'yes': 'no'; ?></td>
+					<td><?= $job->business ?></td>
+					<td><?= $job->employer ?></td>
+					<td><?= $job->employer_type ?></td>
+					<td><?= $job->job_title ?></td>
+					<td><?= $job->minimum.' - '.$job->maximum ?></td>
+					<?php $year_ended = ($job->year_ended == '100000') ? 'ongoing' : $job->year_ended; ?>
+					<td><?= $job->year_started.' - '.$year_ended ?></td>
+					<td><?= ($job->job_satisfaction == '1') ? 'yes': 'no'; ?></td>
+					<td><?= $job->reason ?></td>
+				</tr>
+			<?php endfor; ?>
+			
+			<?php $count++; ?>
+		<?php endforeach; ?>
+
+	</table>
+</body>
+</html>
